9f92e622c2349f7a0d676d8a70590e67c8f44da6
Samuel Vedrik <samuelvedrik@gmail.com>
2021-12-01 00:19:52 -0500

    Added numpy variant solutions

--- day1.py
+++ day1.py
@@ -1,30 +1,35 @@
 import numpy as np
+from scipy.signal import correlate
 
 def load_input(path): 
     with open(path) as file: 
         input_ = file.read()
-    return input_.split("\n")
+    inputs = input_.split("\n")
+    inputs = np.array([int(i) for i in inputs])
+    return inputs
 
-def part_one():
-    inputs = load_input("inputs/day1.txt")
-    inputs = [int(i) for i in inputs]
+def part_one(inputs):
     sum_ = 0
-    for prev, next_ in zip(inputs[:-1], inputs[1:]): 
-        sum_ += 1 if next_ > prev else 0
-    print(sum_)
+    for prev, next_ in zip(inputs, inputs[1:]): 
+        sum_ += next_ > prev
+    return sum_
+
+def numpy_part_one(inputs): 
+    return ((inputs[1:] - inputs[:-1]) > 0).sum()
     
-def part_two():
-    inputs = load_input("inputs/day1.txt")
-    inputs = [int(i) for i in inputs]
-    arr = np.array(inputs)
-    values = arr[:-2] + arr[1:-1] + arr[2:]
+def part_two(inputs):
+    values = inputs[:-2] + inputs[1:-1] + inputs[2:]
     sum_ = 0
-    for prev, next_ in zip(values[:-1], values[1:]): 
-        sum_ += 1 if next_ > prev else 0
-    print(sum_)
-    
+    for prev, next_ in zip(values, values[1:]): 
+        sum_ += next_ > prev
+    return sum_
+
+def numpy_part_two(inputs): 
+    values = inputs[:-2] + inputs[1:-1] + inputs[2:]
+    return ((values[1:] - values[:-1]) > 0).sum()
 
 if __name__ == "__main__": 
-    part_one()
-    part_two()
+    inputs = load_input("inputs/day1.txt")
+    print(numpy_part_one(inputs))
+    print(numpy_part_two(inputs))
     
